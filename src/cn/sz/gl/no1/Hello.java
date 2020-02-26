97cfa9d3e3a7af64e947b72b44c33fdb1d3c6143
gl2001 <45632311@qq.com>
2020-02-26 09:36:09 +0800

    保存本地

--- src/cn/sz/gl/no1/Hello.java
+++ src/cn/sz/gl/no1/Hello.java
@@ -1,10 +1,17 @@
 package cn.sz.gl.no1;
-
+/**
+ * 1.客户端，必须先更新（pull）,再提交(commit and push)
+ * 2.再提交的时候，不要全项目一起提交，写了哪个文件，就只提交哪个文件;
+ * 
+ * @author Administrator
+ *
+ */
 public class Hello {
 
 	public void show() {
 		System.out.println("hello");
 		System.out.println("world");
 		System.out.println("你好");
+		System.out.println("2222222");
 	}
 }
