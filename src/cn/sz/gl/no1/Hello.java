efd9aa819c0504ffb1150484ddf10fc877018081
gl2001 <45632311@qq.com>
2020-02-26 10:21:16 +0800

    给hello.java写了注释

--- src/cn/sz/gl/no1/Hello.java
+++ src/cn/sz/gl/no1/Hello.java
@@ -3,6 +3,16 @@ package cn.sz.gl.no1;
  * 1.客户端，必须先更新（pull）,再提交(commit and push)
  * 2.再提交的时候，不要全项目一起提交，写了哪个文件，就只提交哪个文件;
  * 
+ * 
+ * 当服务器上代码与eclipse里面代码不一致的时候，此时eclipse里面更新服务器(pull的时候)，会出现pull失败;
+ * 此时：
+ * 	1.需要先把eclipse里面的代码commit到本地git库（C:\Users\Administrator\git\gitweb\src\cn\sz\gl\no1）；
+ * 	2.再次从服务器上，pull代码到本地，此时会体现冲突：
+ * 		列举服务器上代码与eclipse里面代码不一致的地方
+ * 		这里需要我们人为解决冲突；
+ * 	3.解决完冲突，我们再次重新提交代码，此时就可以把本地自己新写的代码同步到服务器了，本地库也同时得到更新;
+ * 
+ * 
  * @author Administrator
  *
  */
